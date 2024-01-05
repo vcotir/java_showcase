7e79cc7b5224d9a1e6ea3018135b1cc25d627e63
Victor N <victorkjngo@gmail.com>
2024-01-05 00:17:17 -0800

    Add WebSockets project

---
+++ JavaWebSockets/src/test/java/com/final_413/Na/NaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_413.Na;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
